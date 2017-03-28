01ba11be8870f5496bc277e637f57684d9568665
碱咖啡 <Wll001992Yx@163.com>
2017-03-28 15:08:38 +0800

    Update flexible.js

--- flexible.js
+++ flexible.js
@@ -1,18 +1,7 @@
 /**
- * author: WangLe
+ * author: 碱咖啡
  * github: [https://github.com/jiankafei]
  * 博客: [https://jiankafei.github.io/] 博客小白，在搭建的时候不只什么原因，博客能打开，但文章点击会404
- * 不需要在外部写 meta>name=viewport
- * 如果需要强制设置 dpr，则在html上设置 data-dpr 属性即可
- * 在布局时，一定要使用响应式布局，因为最大字体宽度为540。使用flexbox最好。
- * html的字体大小不能设置为百分比方式，会有bug，详情见 caniuse search>rem
- * 为了在pc上得到更好的体验，pc上的宽度定为414px，不能缩放，并且开发者需为 position:fixed元素 手动添加 fixed类名
- * 点击等事件推荐使用 PEP.js [https://code.jquery.com/pep/0.4.2/pep.js] allowTouch [http://alloyteam.github.io/] v-tap [https://github.com/MeCKodo/vue-tap]
- * 作者自己实现该项目。不兼容 vw 单位；改变ds为设计稿大小；单位换算 yrem = xpx / 100；如果要使用单位为px的字体，推荐使用css解析器(scss,stylus)的mixin完成
- * 部分参考开源项目 lib-flexible.js [https://github.com/amfe/lib-flexible]
- * 参考lib-flexible部分: 添加 window.lib.flexible 对象；添加 pageshow 事件；为事件添加节流操作；最大字体宽度改为540；为body添加默认字体大小
- * 最好内联放到head所有资源的前面
- * 条件有限，测试的机器不多，如果某位仁兄在某些机器上发现问题，希望能告诉我或者fork
  */
 ;(function(G, lib){
 	'use strict';
@@ -27,8 +16,11 @@
 		flexible = lib.flexible || (lib.flexible = {}),
 		tid = null,
 		dt = deviceType(),
-		getW = dt === 'ios' ? function(){return G.screen.width} : function(){return de.offsetWidth / dpr};
+		getW = function(){return de.getBoundingClientRect().width / dpr}; // 宽度
 
+	/* 在红米上，screen的宽等于Rect的宽度，screen取值有问题，使用Rect取值；在安卓中，QQ浏览器、QQwebview、微信webview、支付宝webview、Nexus5x等部分机型里的Rect的宽度取值小于准确值，使用screen取值 */
+	dt !== 'pc' && de.getBoundingClientRect().width / dpr < G.screen.width && (getW = function(){return G.screen.width});
+	// 为html元素添加data-dpr属性
 	de.setAttribute('data-dpr', dpr);
 	// pc上隐藏滚动条，宽度为414，并且为html和定位fixed元素添加宽度
 	dt === 'pc' && addStylesheetRules('::-webkit-scrollbar{display: none !important}html, .fixed{margin-left: auto !important;margin-right: auto !important;width: '+ 414 * dpr +'px !important;');
@@ -43,10 +35,10 @@
 	// 改变窗口
 	G.addEventListener('resize', function () {
 		clearTimeout(tid);
-		tid = setTimeout(trans, 300);
+		tid = G.setTimeout(trans, 300);
 	}, false);
 	G.addEventListener('pageshow', function (e) {
-		e.persisted && (clearTimeout(tid), tid = setTimeout(trans, 300));
+		e.persisted && (clearTimeout(tid), tid = G.setTimeout(trans, 300));
 	}, false);
 
 	// 为body添加默认字体大小
@@ -57,16 +49,16 @@
 	// 执行转换
 	trans();
 
-	// 添加属性
+	// 为flexible对象添加属性
 	flexible.dpr = G.dpr = dpr;
 	flexible.trans = trans;
 	flexible.rem2px = function(d){
-		var val = parseFloat(d) * this.rem;
+		var val = G.parseFloat(d) * this.rem;
 		'string' === typeof d && d.match(/rem$/) && (val += 'px');
 		return val;
 	};
 	flexible.px2rem = function(d){
-		var val = parseFloat(d) / this.rem;
+		var val = G.parseFloat(d) / this.rem;
 		'string' === typeof d && d.match(/px$/) && (val += 'rem');
 		return val;
 	};
@@ -74,7 +66,7 @@
 	function trans(){
 		var w = getW();
 		w > maxW && (w = maxW);
-		var rem = Math.floor(dpr * w * 100 / ds);
+		var rem = G.parseFloat(dpr * w * 100 / ds);
 		de.style.fontSize = rem + 'px';
 		flexible.rem = G.rem = rem;
 	};
